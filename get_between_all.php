3dfc2149858b6aa4b11d57874d332980c61e07e6
Patrik Johansson <popeen@users.noreply.github.com>
2014-04-24 15:45:48 +0200

    Is now based on get_between(). This should fix some errors with special characters.

--- get_between_all.php
+++ get_between_all.php
@@ -1,7 +1,12 @@
 <?php
-	function get_between_all($content, $start, $end){
-		$pattern = "/{$start}(.*?){$end}/";
-		preg_match_all($pattern, $content, $matches);
-		return $matches[1];
+	function get_between_all($content,$start,$end,$array = array()){
+		$r = explode($start, $content, 2);
+		if(isset($r[1])){
+			$r = explode($end, $r[1], 2);
+			$array[] = $r[0];
+			return get_between_all($r[1],$start,$end,$array);
+		} else{
+			return $array;
+		}
 	}
 ?>
